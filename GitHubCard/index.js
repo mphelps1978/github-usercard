9c072a7eaaee7ab52742acfb31e561c14979cabd
mphelps1978 <phelps.michael@live.com>
2019-12-05 18:01:03 -0600

    working on Component Structure

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,25 +1,28 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// const data = axios.get('https://api.github.com/users/mphelps1978');
+// console.log(data);
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
@@ -35,7 +38,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -46,7 +49,45 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
+function CreateProfile(data) {
+	const card = document.createElement('div'),
+		profileImage = document.createElement('img'),
+		cardInfo = document.createElement('div'),
+		realName = document.createElement('h3'),
+		userName = document.createElement('p'),
+		location = document.createElement('p'),
+		profile = document.createElement('p'),
+		githubLink = document.createElement('a'),
+		followers = document.createElement('p'),
+		following = document.createElement('p'),
+		bio = document.createElement('p');
+
+	card.appendChild(profileImage);
+	card.appendChild(cardInfo);
+	cardInfo.appendChild(realName);
+	cardInfo.appendChild(userName);
+	cardInfo.appendChild(location);
+	cardInfo.appendChild(profile);
+	profile.appendChild(githubLink);
+	cardInfo.appendChild(followers);
+	cardinfo.appendChild(following);
+	cardinfo.appendChild(bio);
+
+	card.classList.add('card');
+	cardInfo.classList.add('card-info');
+	realName.classList.add('name');
+
+	profileImage.src = data.avatar_url;
+  realName.textContent = `Name: ${data.name}`;
+  userName.textContent = `UserName: ${data.login}`;
+  location.textContent = `Location: ${data.location}`;
+  profile.textContent = 'Profile:';
+
+
+
+}
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
